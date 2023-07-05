f6ac8c53ca38a3067d56910e1d1a84d4d020e761
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-07-05 16:09:35 +0100

    Set up morgan, body-parser, cors middlewares

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser') ;
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 
 app.get('/api/customers', cors(), (req, res) => {
   const customers = [
