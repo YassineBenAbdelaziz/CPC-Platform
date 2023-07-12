d9f15c1be2073f275b64927e8b3c63f974ca6382
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-07-12 11:25:55 +0100

    Create contest routes

---
+++ server/Routes/contest.js
@@ -0,0 +1,17 @@
+const contest = require('../Entities/contest');
+const controller = require('../Controllers/contest')
+const express = require('express');
+const router = express.Router();
+
+router.get('/', controller.getContests);
+
+router.get('/:id', controller.getContestById);
+
+router.post('/add', controller.createContest );
+
+router.delete('/:id',controller.deleteContest);
+
+router.patch('/:id',controller.updateContest);
+
+
+module.exports = router;
