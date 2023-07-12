e48913c4d31c243849e42ed4ec94419a3fc9670e
Rami Jebali <jebalirami7@gmail.com>
2023-07-13 00:41:01 +0100

    Create example contoller

---
+++ server/Controllers/example.js
@@ -0,0 +1,96 @@
+const { models } = require('../sequelize');
+
+
+exports.get_all = async (req, res, next) => {
+    await models.example.findAll()
+        .then((results) => {
+            res.status(200).json(results);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
+}
+
+
+exports.create_example = async (req, res, next) => {
+    const example = {
+        input: req.body.input,
+        output: req.body.output
+    }
+
+    await models.example.create(example)
+        .then(res.status(201).json({
+            message: "Example Created",
+            example: example
+        }))
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+}
+
+
+exports.get_example = async (req, res, next) => {
+    const id = req.params.exampleId;
+    await models.example.findByPk(id)
+        .then(example => {
+            if (!example) {
+                return res.status(404).json({
+                    message: "example NOT FOUND"
+                });
+            } else {
+                res.status(200).json(example);
+            }
+        }).catch(err => {
+            res.status(500).json({ error: err });
+        });
+}
+
+
+exports.update_example = async (req, res, next) => {
+    const id = req.params.exampleId;
+
+    await models.example.findByPk(id)
+        .then(async example => {
+            if (!example) {
+                return res.status(404).json({
+                    message: "example NOT FOUND"
+                });
+            } else {
+                const exampleBody = {
+                    input: req.body.input,
+                    output: req.body.output
+                }
+
+                await models.example.update(exampleBody, { where: { id_example: id } })
+                    .then(() => {
+                        res.status(200).json({
+                            message: "Updated example with id : " + id
+                        });
+                    })
+            }
+        }).catch(err => {
+            res.status(500).json({ error: err });
+        });
+}
+
+
+exports.delete_example = async (req, res, next) => {
+    const id = req.params.exampleId;
+    await models.example.findByPk(id)
+        .then(async example => {
+            if (!example) {
+                return res.status(404).json({
+                    message: "example NOT FOUND"
+                });
+            } else {
+                await models.example.destroy({ where: { id_example: id } })
+                    .then(() => {
+                        res.status(200).json({
+                            message: "Deleted example with id : " + id
+                        });
+                    })
+            }
+        }).catch(err => {
+            res.status(500).json({ error: err });
+        });
+}
