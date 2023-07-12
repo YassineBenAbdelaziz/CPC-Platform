c53c3c65f6781942d2ae917350ad409bb08e033c
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-07-12 11:25:27 +0100

    Create contest service

---
+++ server/Controllers/contest.js
@@ -0,0 +1,96 @@
+const {models} = require('../sequelize');
+
+
+exports.getContests = (req, res, next) => {
+    models.contest.findAll()
+    .then( (results) => {
+        res.status(200).json(results);
+    })
+    .catch((err) => {
+        res.status(500).json({error : err});
+    });
+};
+
+
+
+
+exports.getContestById = (req, res, next) => {
+    models.contest.findByPk(req.params.id)
+    .then( (results) => {
+        res.status(200).json(results);
+    })
+    .catch((err) => {
+        res.status(500).json({error : err});
+    });
+};
+
+
+
+
+
+exports.createContest = (req, res, next) => {
+    models.contest.create({
+        title: req.body.title,
+        status : req.body.status,
+        date : req.body.date,
+        start : req.body.start,
+        end : req.body.end,
+    })
+    .then( (data) => {
+        res.status(201).json({
+            message : "Create Successful",
+        });
+    })
+    .catch( (err) => {
+        res.status(500).json({
+            error : err,
+        });
+    });
+};
+
+
+
+exports.deleteContest = (req, res , next) => {
+    models.contest.destroy({
+        where : {
+            id_contest : req.params.id,
+        }
+    })
+    .then( (data) => {
+        res.status(201).json({
+            message : "Delete Successful",
+        });
+    })
+    .catch( (err) => {
+        res.status(500).json({
+            error : err,
+        });
+    });
+};
+
+
+
+exports.updateContest = (req, res , next) => {
+    const updatedFields = {} ;
+    console.log(req.body)
+    for (const [key,val]of Object.entries(req.body)) {
+        updatedFields[key] = val;
+    }
+
+    models.contest.update(updatedFields,
+    {
+        where : {
+            id_contest : req.params.id,
+        }
+    })
+    .then( (data) => {
+        res.status(201).json({
+            message : "Update Successful",
+        });
+    })
+    .catch( (err) => {
+        res.status(500).json({
+            error : err,
+        });
+    });
+};
