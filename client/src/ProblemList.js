b39c08ff6d62659f9d5027ced0fd71588c0da9ac
Rami Jebali <jebalirami7@gmail.com>
2023-07-16 14:57:53 +0100

    Add pagination modifications

--- client/src/ProblemList.js
+++ client/src/ProblemList.js
@@ -7,13 +7,30 @@ const ProblemList = ({ problemset, title, titlePrefixe = false, contestId = 0 })
         return titlePrefixe ? `/contests/${contestId}/${x}` : `/problemset/${x}`;
     }
 
+    const diff = (score) => {
+        if (score <= 100) {
+            return "easy"
+        } else if (score > 100 && score < 120) {
+            return "meduim"
+        } else {
+            return "hard"
+        }
+    }
+
     return (
         <div className="problem-list">
             <h2>{title}</h2>
+            <div className="table-titles">
+                <h3 className="title">Title</h3>
+                <h3 className="diff">Difficulty</h3>
+                <h3 className="score">Score</h3>
+            </div>
             {problemset.sort((a, b) => a.score > b.score ? 1 : -1).map((problem, index) => (
                 < Link to={fn(problem.id_problem)} key={index}>
                     <div className="problem">
                         <h2>{titlePrefixe && (ch[index] + '.')} {problem.title}</h2>
+                        <div className="problem-diff" style={{ color: 'black' }}>{diff(problem.score)}</div>
+                        <div className="problem-score">{problem.score}</div>
                     </div>
                 </Link>
             ))
--- client/src/Problemset.js
+++ client/src/Problemset.js
@@ -1,14 +1,29 @@
 import useFetch from "./useFetch";
 import ProblemList from "./ProblemList";
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 const Problemset = () => {
     const { data: problemset, isPending, error } = useFetch('http://localhost:5000/problem');
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [problemsPerPage] = useState(10);
+    problemset && console.log(problemsPerPage, problemset.length);
+
+    //Get Current Problems
+    const indexOfLastProblem = currentPage * problemsPerPage;
+    const indexOfFirstProblem = indexOfLastProblem - problemsPerPage;
+    const currentProblems = problemset && problemset.slice(indexOfFirstProblem, indexOfLastProblem);
+
+    //Change Page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div className="problemset">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {problemset && <ProblemList problemset={problemset} title="Problems" />}
+            {problemset && <ProblemList problemset={currentProblems} title="Problems" />}
+            {problemset && problemset.length > 10 && <Pagination postsPerPage={problemsPerPage} totalPosts={problemset.length} paginate={paginate} />}
         </div>
     );
 }
