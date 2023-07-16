24d31283294dce329373b1b4435c82ef2614c121
Rami Jebali <jebalirami7@gmail.com>
2023-07-16 16:23:43 +0100

    Sort problems in a contest

--- client/src/ContestDetails.js
+++ client/src/ContestDetails.js
@@ -9,6 +9,9 @@ const ContestDetails = () => {
     // eslint-disable-next-line
     const { data: problems, isPending1, error1 } = useFetch('http://localhost:5000/problem/findByContest/' + id);
 
+    //Sorting problems 
+    problems && problems.sort((a, b) => a.score > b.score ? 1 : -1);
+
     return (
         <div className="contest-details">
             {error && <div>{error}</div>}
