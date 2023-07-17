5aa20665d9604bc7326fe22e3c8cdf9d88502b22
Rami Jebali <jebalirami7@gmail.com>
2023-07-17 20:10:59 +0100

    Add colors to difficulties

--- client/src/ProblemList.js
+++ client/src/ProblemList.js
@@ -9,19 +9,27 @@ const ProblemList = ({ problemset, title, titlePrefixe = false, contestId = 0 })
 
     const diff = (score) => {
         if (score <= 100) {
-            return "easy"
-        } else if (score > 100 && score < 120) {
-            return "meduim"
+            return (
+                <div className="problem-diff" style={{ color: '#02b102' }} >easy</div>
+            )
+        } else if (score > 100 && score < 130) {
+            return (
+                <div className="problem-diff" style={{ color: 'orange' }} >miduim</div>
+            )
         } else {
-            return "hard"
+            return (
+                <div className="problem-diff" style={{ color: 'red' }} >hard</div>
+            )
         }
     }
+
 
     return (
         <div className="problem-list">
             <h2>{title}</h2>
             <div className="table-titles">
                 <h3 className="title">Title</h3>
+                <h3 className="status">Status</h3>
                 <h3 className="diff">Difficulty</h3>
                 <h3 className="score">Score</h3>
             </div>
@@ -29,7 +37,8 @@ const ProblemList = ({ problemset, title, titlePrefixe = false, contestId = 0 })
                 < Link to={fn(problem.id_problem)} key={index}>
                     <div className="problem">
                         <h2>{titlePrefixe && (ch[index] + '.')} {problem.title}</h2>
-                        <div className="problem-diff" style={{ color: 'black' }}>{diff(problem.score)}</div>
+                        <div className="problem-status">{ }</div>
+                        {diff(problem.score)}
                         <div className="problem-score">{problem.score}</div>
                     </div>
                 </Link>
