cbea633d1d891a7ca0a0da517e311af4268b9676
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-07-25 12:08:37 +0100

    Set up passport, sessions and redis store

--- server/app.js
+++ server/app.js
@@ -10,6 +10,58 @@ const example = require('./Routes/example');
 const submission = require('./Routes/submission');
 const tag = require('./Routes/tag');
 const user = require('./Routes/user');
+const passport = require('passport');
+const session = require('express-session');
+const RedisStore = require("connect-redis").default
+const {createClient} = require('redis');
+
+
+/*
+* ---------------- Redis Store SETUP ----------------
+*/
+
+let redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+let redisStore = new RedisStore({
+  client: redisClient,
+});
+
+
+
+/*
+* ---------------- SESSION SETUP ----------------
+*/
+
+app.use(session({
+  secret : process.env.SECRET_SESSION ,
+  resave: false,
+  saveUninitialized: false,
+  store : redisStore,
+  cookie : {
+    maxAge: 1000 * 60 * 60 * 24,
+  }
+}));
+
+
+/*
+* ---------------- Passport Init ----------------
+*/
+
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+/*
+* ---------------- Cors, bodyParser ... ----------------
+*/
+
+
+
 
 app.use(morgan('dev'));
 app.use(cors());
@@ -29,12 +81,22 @@ app.use((req, res, next) => {
 
 
 
+/*
+* ---------------- Routes ----------------
+*/
+
+
 app.use("/contest", contest);
 app.use('/problem', problem);
 app.use('/example', example);
 app.use('/submission', submission);
 app.use('/tag', tag);
 app.use('/user', user);
+
+
+/*
+* ---------------- Error Handler ----------------
+*/
 
 app.use((req, res, next) => {
   const error = new Error("NOT FOUND");
