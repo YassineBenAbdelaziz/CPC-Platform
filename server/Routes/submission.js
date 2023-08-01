7bd32fa0f9313f01f02b48e6ce5582872061543c
Rami Jebali <jebalirami7@gmail.com>
2023-08-01 15:56:42 +0100

    Modify submission routes

--- server/Routes/submission.js
+++ server/Routes/submission.js
@@ -7,6 +7,8 @@ router.get('/', SubmissionController.get_all);
 
 router.post('/', SubmissionController.create_submission);
 
+router.get('/findByProblem/:problemId', SubmissionController.get_submissions_by_problem);
+
 router.get('/:submissionId', SubmissionController.get_submission);
 
 module.exports = router; 
