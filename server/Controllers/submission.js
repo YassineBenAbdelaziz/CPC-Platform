339254e68dcec52d96ffbe6d78f8b453b17483ee
Rami Jebali <jebalirami7@gmail.com>
2023-08-07 12:10:00 +0100

    Add get subs by problemAndUser controllerAndRoutes

--- server/Controllers/submission.js
+++ server/Controllers/submission.js
@@ -1,4 +1,4 @@
-const submission = require('../Entities/submission');
+const sequelize = require('sequelize');
 const { models } = require('../sequelize');
 const axios = require("axios");
 
@@ -10,6 +10,7 @@ exports.get_all = async (req, res, next) => {
             res.status(200).json(results);
         })
         .catch((err) => {
+            console.log(err)
             res.status(500).json({ error: err });
         });
 }
@@ -23,58 +24,66 @@ exports.create_submission = async (req, res, next) => {
         result: "In Queue",
         time: 0,
         memory: 0,
-        problemIdProblem: req.body.problemId
+        problemIdProblem: req.body.problemId,
+        userIdUser: req.body.userId
     }
 
-    const problemId = submission.problemIdProblem
-    let timeLimit = 0;
-    let memoryLimit = "";
-    let testFile = "";
-    await models.problem.findByPk(problemId)
-        .then(data => {
-            const problem = data.dataValues;
-            timeLimit = problem.time_limit;
-            memoryLimit = problem.memory_limit;
-            testFile = problem.test_file;
-        })
+    if (req.body.langId === 50) {
+        submission.lang = "C"
+    } else if (req.body.langId === 54) {
+        submission.lang = "C++"
+    } else if (req.body.langId === 62) {
+        submission.lang = "Java"
+    } else if (req.body.langId === 71) {
+        submission.lang = "Python"
+    }
 
-    const tests = testFile.split("\nexpected\n");
-    const inputs = tests[0].split("\nnext\n");
-    const outputs = tests[1].split("\nnext\n");
+    if (submission.code !== "") {
+        const problemId = submission.problemIdProblem
+        let timeLimit = 0;
+        let memoryLimit = "";
+        let testFile = "";
+        await models.problem.findByPk(problemId)
+            .then(data => {
+                const problem = data.dataValues;
+                timeLimit = problem.time_limit;
+                memoryLimit = problem.memory_limit;
+                testFile = problem.test_file;
+            })
 
-    let bodies = []
+        const tests = testFile.split("\nexpected\n");
+        const inputs = tests[0].split("\nnext\n");
+        const outputs = tests[1].split("\nnext\n");
 
-    for (let i = 0; i < inputs.length - 1; i++) {
-        const body = {
-            "source_code": submission.code,
-            "language_id": req.body.langId,
-            "memory_limit": memoryLimit,
-            "cpu_time_limit": timeLimit,
-            "stdin": inputs[i],
-            "expected_output": outputs[i]
+        let bodies = []
+
+        for (let i = 0; i < inputs.length - 1; i++) {
+            const body = {
+                "source_code": submission.code,
+                "language_id": req.body.langId,
+                "memory_limit": memoryLimit,
+                "cpu_time_limit": timeLimit,
+                "stdin": inputs[i],
+                "expected_output": outputs[i]
+            }
+            bodies.push(body)
         }
-        bodies.push(body)
-    }
 
-    let tokens = "";
-    await axios.post(jugdeUrl + "submissions/batch/?base64_encoded=false", {
-        "submissions": bodies
-    }).then(result => {
-        console.log('Post submission to judge successful')
-        result.data && result.data.map(item => tokens += item.token + ",")
-        submission.tokens = tokens;
-    }).catch(err => {
-        console.log('Post submission to judge failed')
-        console.log(err)
-    });
-
-    await axios.get(jugdeUrl + "languages/" + bodies[0].language_id)
-        .then(result => {
-            submission.lang = result.data.name;
+        let tokens = "";
+        await axios.post(jugdeUrl + "submissions/batch/?base64_encoded=false", {
+            "submissions": bodies
+        }).then(result => {
+            console.log('Post submission to judge successful')
+            result.data && result.data.map(item => tokens += item.token + ",")
+            submission.tokens = tokens;
         }).catch(err => {
-            console.log('Get language name from judge failed')
+            console.log('Post submission to judge failed')
             console.log(err)
         });
+
+    } else {
+        submission.result = "No Code"
+    }
 
     await models.submission.create(submission)
         .then(res.status(201).json({
@@ -83,6 +92,7 @@ exports.create_submission = async (req, res, next) => {
         }))
         .catch(error => {
             console.log('Create submission failed')
+            console.log(error)
             res.status(500).json({ error: error });
         });
 }
@@ -103,10 +113,7 @@ get_submission_details = async (tokens) => {
                 subResult.memory = Math.max(subResult.memory, sub[i].memory)
                 subResult.result = sub[i].status.description
                 subResult.result = subResult.result.includes('Runtime Error') ? "Runtime Error" : subResult.result
-                if (subResult.result !== "Accepted" && subResult.result !== "In Queue") {
-                    subResult.result !== "Processing" ? subResult.result += " On" : subResult.result += ""
-                    subResult.result += " Test Case " + (i + 1);
-                    console.log(subResult)
+                if (subResult.result !== "Accepted" && subResult.result !== "In Queue" && subResult.result !== "Processing") {
                     break;
                 }
             }
@@ -118,45 +125,129 @@ get_submission_details = async (tokens) => {
 }
 
 
-exports.get_submissions_by_problem = async (req, res, next) => {
-    let sub = {};
-    await models.submission.findOne({
+exports.get_submissions_by_problem_and_user = async (req, res, next) => {
+    let subs = [];
+    await models.submission.findAll({
         where: {
+            userIdUser: req.params.userId,
             problemIdProblem: req.params.problemId,
-            result: "In Queue"
+            [sequelize.Op.or]: [
+                { result: { [sequelize.Op.like]: "Processing%" } },
+                { result: "In Queue" }
+            ]
+
         }
-    }).then(submission => {
-        sub = submission && submission.dataValues
+    }).then(submissions => {
+        submissions.map(sub => {
+            subs.push(sub.dataValues)
+        })
     })
 
-    if (sub && sub.id_submission) {
-        const subResult = await get_submission_details(sub.tokens);
-        await models.submission.update(subResult, { where: { id_submission: sub.id_submission } })
+    for (let sub of subs) {
+        if (sub && sub.id_submission) {
+            const subResult = await get_submission_details(sub.tokens);
+            await models.submission.update(subResult, { where: { id_submission: sub.id_submission } })
+        }
     }
 
+    let username = ""
+    await models.user.findByPk(req.params.userId)
+        .then(user => {
+            username = user.dataValues.username
+        }).catch(err => {
+            console.log('User Not Found')
+            res.status(404).json(err)
+        })
+
+    await models.submission.findAll({
+        where: {
+            userIdUser: req.params.userId,
+            problemIdProblem: req.params.problemId
+        }
+    }).then(async submissions => {
+        let result = []
+        for (let sub of submissions) {
+            sub.dataValues.user = username
+            result.push(sub.dataValues)
+        }
+        res.status(200).json(result)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+
+}
+
+
+exports.get_submissions_by_problem = async (req, res, next) => {
     await models.submission.findAll({
         where: {
             problemIdProblem: req.params.problemId
         }
-    }).then(submissions => {
-        res.status(200).json(submissions)
+    }).then(async submissions => {
+        let result = []
+        for (let sub of submissions) {
+            await models.user.findByPk(sub.dataValues.userIdUser)
+                .then(user => {
+                    sub.dataValues.user = user.dataValues.username
+                    result.push(sub.dataValues)
+                }).catch(err => {
+                    console.log('User Not Found')
+                    res.status(404).json(err)
+                })
+        }
+        res.status(200).json(result)
     })
-
 }
 
 
 exports.get_submission = async (req, res, next) => {
     const id = req.params.submissionId;
     await models.submission.findByPk(id)
-        .then(submission => {
+        .then(async submission => {
             if (!submission) {
                 return res.status(404).json({
                     message: "Submission NOT FOUND"
                 });
             } else {
-                res.status(200).json(submission);
+                let result = []
+                await axios.get(jugdeUrl + `submissions/batch?tokens=${submission.dataValues.tokens}&base64_encoded=false&fields=stdin,stdout,expected_output,status,time,memory`)
+                    .then(res => {
+                        console.log('Get submission from judge successful')
+                        const sub = res.data.submissions;
+                        for (let i = 0; i < sub.length; i++) {
+                            const subResult = {
+                                time: 0,
+                                memory: 0,
+                                stdin: "",
+                                stdout: "",
+                                expected_output: "",
+                                result: ""
+                            }
+                            subResult.time = sub[i].time * 1000
+                            subResult.memory = sub[i].memory
+                            subResult.stdin = sub[i].stdin
+                            subResult.stdout = sub[i].stdout
+                            subResult.expected_output = sub[i].expected_output
+                            subResult.result = sub[i].status.description
+                            subResult.result = subResult.result.includes('Runtime Error') ? "Runtime Error" : subResult.result
+                            result.push(subResult)
+                            if (subResult.result !== "Accepted" && subResult.result !== "In Queue" && subResult.result !== "Processing") {
+                                break;
+                            }
+                        }
+                    }).catch(err => {
+                        console.log('Get submission from judge failed')
+                        console.log(err)
+                    });
+                console.log(result)
+                res.status(200).json({
+                    count: result.length,
+                    testCases: result
+                });
             }
         }).catch(err => {
+            console.log(err)
             res.status(500).json({ error: err });
         });
 }
--- server/Routes/submission.js
+++ server/Routes/submission.js
@@ -9,6 +9,8 @@ router.post('/', SubmissionController.create_submission);
 
 router.get('/findByProblem/:problemId', SubmissionController.get_submissions_by_problem);
 
+router.get('/findByProblemAndUser/:problemId/:userId', SubmissionController.get_submissions_by_problem_and_user);
+
 router.get('/:submissionId', SubmissionController.get_submission);
 
 module.exports = router; 
