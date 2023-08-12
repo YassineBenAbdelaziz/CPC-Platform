9b4b758f48c2dcfd5db6a9c9ff33717eb2c1d624
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-08-12 21:16:39 +0100

    Refresh user data on page refresh

---
+++ client/src/RefreshUser.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import useAuth from "./hooks/useAuth";
+import Axios from 'axios'
+
+
+const RefrechUser = ({children}) => {
+
+    const [isLoading, setIsLoading] = useState(true) ;
+    const { auth, setAuth } = useAuth();
+
+    const url = "http://localhost:5000/";
+    Axios.defaults.withCredentials = true;
+
+    useEffect( () => {
+        
+        const refreshData = () => {
+            Axios.get(url +  'user/current').then( res => {
+                if (res.data.status === 'OK') {
+                    setAuth(res.data.data);
+                    console.log("Refreshing data");
+                }
+                setIsLoading(false);
+            }).catch( err => {
+                console.log(err);
+                setIsLoading(false);
+            });
+        };
+        
+        !auth?.username ? refreshData() : setIsLoading(false);
+    },[]); 
+
+    return (
+        <>
+            {isLoading ? 
+            <p>Loading...</p>
+            : <>
+            {children}
+            </>
+            }
+        </>
+        )
+}
+
+export default RefrechUser ;
