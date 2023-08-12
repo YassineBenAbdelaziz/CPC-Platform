686d8f855f75eab6fa546c9cbe0fcfa03f1c42f3
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-08-12 21:16:54 +0100

    Update component tree

--- client/src/App.js
+++ client/src/App.js
@@ -15,44 +15,49 @@ import ProtectedRoutes from './ProtectedRoutes';
 import ShowNavBar from './ShowNavbar';
 import AddProblem from './AddProblem';
 import Profile from './Profile';
+import RefrechUser from './RefreshUser';
+
+
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App" style={{ minHeight: '633px' }}>
-        <ShowNavBar>
-          <NavBar />
-        </ShowNavBar>
-        <div className="app-content">
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/profile' element={<Profile />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/signup' element={<SignUp />}></Route>
-            <Route path='/problemset' element={<Problemset />}></Route>
-            <Route path='/problemset/:id' element={<ProblemDetails />}></Route>
-            <Route path='/contests/:id/:id' element={<ProblemDetails />}></Route>
-            <Route path='/contests/:id' element={<ContestDetails />}></Route>
-            <Route path='/contests' element={<Contests />}></Route>
-            <Route element={<ProtectedRoutes />}>
-              {/* protected routes here */}
-              <Route path='/problemset/add-problem' element={<AddProblem />}></Route>
-              <Route path='/loggedin' element={<LoggedIn />}></Route>
-            </Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Routes>
+      <RefrechUser>
+        <div className="App" style={{ minHeight: '633px' }}>
+          <ShowNavBar>
+            <NavBar />
+          </ShowNavBar>
+          <div className="app-content">
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/about' element={<About />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/signup' element={<SignUp />}></Route>
+              <Route path='/problemset' element={<Problemset />}></Route>
+              <Route path='/problemset/:id' element={<ProblemDetails />}></Route>
+              <Route path='/contests/:id/:id' element={<ProblemDetails />}></Route>
+              <Route path='/contests/:id' element={<ContestDetails />}></Route>
+              <Route path='/contests' element={<Contests />}></Route>
+              <Route element={<ProtectedRoutes />}>
+                {/* protected routes here */}
+                <Route path='/problemset/add-problem' element={<AddProblem />}></Route>
+                <Route path='/loggedin' element={<LoggedIn />}></Route>
+                <Route path='/profile' element={<Profile />}></Route>
+              </Route>
+              <Route path='*' element={<NotFound />}></Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-      <ScrollToTop />
-      <ShowNavBar>
-        <footer>
-          <p>
-            &copy;  2023 CPC Enicarthage <br />
-            All rights reserved.
-          </p>
-        </footer>
-      </ShowNavBar>
+        <ScrollToTop />
+        <ShowNavBar>
+          <footer>
+            <p>
+              &copy;  2023 CPC Enicarthage <br />
+              All rights reserved.
+            </p>
+          </footer>
+        </ShowNavBar>
+        </RefrechUser>
     </BrowserRouter>
   );
 }
