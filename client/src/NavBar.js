3bbdd2af2e58734313d599c3fd96fc475f77c98a
Rami Jebali <jebalirami7@gmail.com>
2023-08-19 14:17:39 +0100

    Change navbar when logged in

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,22 +1,100 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "./imgs/logo-no-bg.png"
+import useAuth from "./hooks/useAuth";
+import user from "./imgs/user.png"
+import settings from "./imgs/settings.png"
+import logout from "./imgs/logout.png"
+import help from "./imgs/question.png"
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 const NavBar = () => {
+    const url = "http://localhost:5000/";
+
+    const { auth, setAuth } = useAuth();
+    const [showProfile, setShowProfile] = useState(false)
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        axios.get(url + 'user/logout').then(res => {
+            console.log(res.data)
+            setAuth({})
+        }).catch(err => {
+            console.log(err)
+        });
+
+        // window.location.reload()
+        setShowProfile(false);
+        document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+        navigate('/login', { replace: true })
+    }
+
+    let menuRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if (auth?.username && !menuRef.current.contains(e.target)) {
+                setShowProfile(false)
+            }
+        }
+        document.addEventListener('mousedown', handler)
+        return () => {
+            document.removeEventListener('mousedown', handler)
+        }
+    })
+
     return (
         <nav className="navbar">
-            <img src={logo} alt="Logo" />
+            <img className="logo" src={logo} alt="Logo" />
             <div className="links">
                 <NavLink to="/" className={({ isActive }) => (isActive ? "active" : 'none')}>Home</NavLink>
                 <NavLink to="/problemset" className={({ isActive }) => (isActive ? "active" : 'none')}>Problemset</NavLink>
                 <NavLink to="/contests" className={({ isActive }) => (isActive ? "active" : 'none')}>Contests</NavLink>
                 <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : 'none')}>About Us</NavLink>
             </div>
-            <div className="auth">
-                <Link to="/login" className="login">Login</Link>
-                <Link to="/signup" className="signup">Sign Up</Link>
-            </div>
+            {auth?.username ?
+                <div className="dropdown" style={{ marginLeft: 'auto' }}>
+                    <img
+                        className="user-img"
+                        src={'http://localhost:5000/' + auth?.img}
+                        alt="user-img"
+
+                        onClick={() => setShowProfile(!showProfile)}
+                    />
+                    <div
+                        className="user-info" ref={menuRef}
+                        style={showProfile ? { display: 'block' } : { display: 'none' }}
+                    >
+                        <Link to={`/profile/${auth?.id}`} className="item" onClick={() => setShowProfile(false)}>
+                            <img src={user} alt="" />
+                            <h4 className="name">Profile</h4>
+                        </Link>
+                        <div className="item">
+                            <img src={settings} alt="" />
+                            <h4 className="name">Settings</h4>
+                        </div>
+                        <hr />
+                        <div className="item">
+                            <img src={help} alt="" />
+                            <h4 className="name">Help Center</h4>
+                        </div>
+                        <hr />
+                        <div className="item" onClick={() => handleLogout()}>
+                            <img src={logout} alt="" />
+                            <h4 className="name">Logout</h4>
+                        </div>
+
+                    </div>
+
+                </div> :
+                <div className="auth">
+                    <Link to="/login" className="login">Login</Link>
+                    <Link to="/signup" id="signup">Sign Up</Link>
+                </div>}
         </nav>
     );
 }
+
 
 export default NavBar;
